75cd7006ebbf51baab474dd361da65b89c936b7a
FininenDynasty <william.fininen@gmail.com>
2025-10-04 23:18:55 -0400

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,21 +1,9 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      "@": path.resolve(__dirname, "src"),
     },
-    configure: (webpackConfig) => {
-      webpackConfig.watchOptions = {
-        ignored: [
-          '**/node_modules/**',
-          '**/.git/**',
-          '**/build/**',
-          '**/dist/**',
-          '**/public/**'
-        ]
-      };
-      return webpackConfig;
-    }
-  }
+  },
 };
